9efe8cffa1e05d10325c050bda1cb808cbb363e4
kzorin52 <kzorin52@gmail.com>
2020-05-31 17:00:05 +0600

    Added class

---
+++ gitTest/TemnijClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitTest
+{
+    class TemnijClass
+    {
+        public void Start()
+        {
+
+        }
+    }
+}
