1de98cea08e39ba16c0a8256a5e60f4fd3e6c8d9
kzorin52 <kzorin52@gmail.com>
2020-06-14 16:18:48 +0600

    123

---
+++ gitTest/Drawing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitTest
+{
+    class Drawing
+    {
+        public void Start(string[,] field)
+        {
+            
+
+        }
+    }
+}
