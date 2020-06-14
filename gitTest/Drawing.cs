fa9f85c0452f946dcf44aa0d6aabf17c2f6d59c9
kzorin52 <kzorin52@gmail.com>
2020-06-14 17:15:37 +0600

    End of Drawing function

--- gitTest/Drawing.cs
+++ gitTest/Drawing.cs
@@ -6,11 +6,29 @@ namespace gitTest
     {
         public void Start(string[,] field)
         {
-            for(int i = field.Length; i > 0; i--) { 
-                Console.WriteLine(field[i,i]);
-            }
-            
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Крестики-Нолики\n");
+            Console.ResetColor();
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+               
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    
+                    Console.Write(" " + field[i, j]);
+                    Console.Write("|");
+                   
 
+                }
+                Console.WriteLine();
+                if(i!=2)
+                {
+                    Console.WriteLine(" ————————");
+                }
+
+            }
+          
         }
     }
 }
